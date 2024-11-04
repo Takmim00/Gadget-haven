0694466b472f595af387adddc45bd8ad2663601d
Takmim00 <takmimm@gmail.com>
2024-11-04 10:31:06 +0600

    all product btn click to all data show

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,7 +6,7 @@ const Categories = ({categories}) => {
     <div className="w-1/4">
       <div className="card bg-white  ">
         <div className="card-body items-center text-center">
-        {/* <Link to={`/category/All`} key={category.id} className="btn w-full  rounded-full bg-purple-600">All products</Link> */}
+        <Link to='/'  className="btn w-full  rounded-full bg-purple-600">All products</Link>
         {
             categories.map(category=>(
                 <Link to={`/category/${category.category}`} key={category.id} className="btn w-full  rounded-full bg-purple-600">{category.category}</Link>
