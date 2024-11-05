fe7abfb51ddd6f37392a3f3334457dae767d3ba4
Takmim00 <takmimm@gmail.com>
2024-11-05 21:01:52 +0600

    navbar location added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import  { Toaster } from 'react-hot-toast';
 
 const MainLayout = () => {
+  const location = useLocation();
+  const backgroundColor = location.pathname === "/" ? "#9333ea" : "#e5e7eb";
   return (
-    <div className="bg-base-200">
-      <Toaster/>
-      <div>
+    <div className="bg-gray-200">
+      <Toaster />
+      <div
+        style={{ backgroundColor }}
+        className="w-11/12 mx-auto rounded-t-2xl mt-3"
+      >
         <Navbar></Navbar>
       </div>
       <div className="min-h-[calc(100vh-426px)] w-11/12 mx-auto">
